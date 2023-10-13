468f25d12a061e7c5c05f5da9b3ffb84da03f339
Ирина Ракова <irakova.education@gmail.com>
2023-10-13 17:45:08 +0300

    Changed commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
     public int calculate(double weightKg, double heightMeter) {
-        double bmi;//индекс массы тела
-
-        bmi = weightKg / (heightMeter * heightMeter);
+        double bmi = weightKg / (heightMeter * heightMeter);
         return (int) bmi;
     }
 }
